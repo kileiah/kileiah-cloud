cb9b42b36271c19fd90b514b12e4c026e7f09c60
Cancy Chu <cancy@Cancys-MacBook-Air.local>
2017-06-17 09:57:01 -0600

    Add static root.

--- webgame/settings.py
+++ webgame/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
