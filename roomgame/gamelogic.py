40541d1c2a8735c0cdc43f7a99565964a3656abd
Cancy Chu <cancy@Cancys-MacBook-Air.local>
2017-06-18 13:22:35 -0600

    Formats roomgame text.

--- roomgame/gamelogic.py
+++ roomgame/gamelogic.py
@@ -34,27 +34,27 @@ I can't believe it! It's a game!
 It's a game in which you pick up a key and escape a room. 
 		"""
 		self.silly_greeting = [
-			"You must love tomato sandwiches.",
+			"you must love tomato sandwiches.",
 			"I like your silly socks.",
 			"I think you'd look snazzy in a hat.",
-			"Don't you just love coconut water?",
-			"Meow! Meow!",
-			"Hug the turtles when you go swimming!"
+			"don't you just love coconut water?",
+			"meow! meow!",
+			"hug the turtles when you go swimming!"
 		]
 		self.action_list = {
-			"think": "Good thinking.",
-			"look": "You look around and spot a key on the floor!",
-			"sit": "The floor's pretty comfy.",
-			"stand": "You stand up.",
+			"think": "hood thinking.",
+			"look": "you look around and spot a key on the floor!",
+			"sit": "the floor's pretty comfy.",
+			"stand": "you stand up.",
 			"dance": "I like your dancing.",
-			"walk": """You walk around the room. 
-	There are walls. 
-	There's a floor. 
-	There's a ceiling. 
-	There's a door.""",
-			"pick": "You pick up the key! It's super shiny.",
-			"unlock": "You unlock the door with the key!",
-			"open": "You escape!"
+			"walk": """you walk around the room. 
+	there are walls. 
+	there's a floor. 
+	there's a ceiling. 
+	there's a door.""",
+			"pick": "you pick up the key! it's super shiny.",
+			"unlock": "you unlock the door with the key!",
+			"open": "you escape!"
 		}
 		self.valid_action = True
 		self.action = None
@@ -138,15 +138,15 @@ Have a great time at the party {}!!!""".format(username))
 		for x in self.session['name']:
 			val += ord(x)
 		val = val % len(self.silly_greeting)
-		return "It looks like your name is {}!\n".format(self.session['name']) + self.silly_greeting[val]
+		return "it looks like your name is {}!\n".format(self.session['name']) + self.silly_greeting[val]
 
 	def get_action_msg(self):
 		if 'name' not in self.session:
-			return "First we need your username, good person!"
+			return "first we need your username, good person!"
 		if self.set_users_name:
 			return self.get_greeting()
 		if not self.valid_action:
-			return "Not a valid action, try again!"
+			return "not a valid action, try again!"
 		if self.action == "think":
 			return self.action_list[self.action]
 		elif self.action == "look":
@@ -157,10 +157,10 @@ Have a great time at the party {}!!!""".format(username))
 				self.session["sitting"] = True
 				return self.action_list[self.action]
 			else:
-				return "You sit even more."
+				return "you sit even more."
 		elif self.action == "stand":
 			if not self.session.get("sitting", False):
-				return "You're already standing."
+				return "you're already standing."
 			else:
 				self.session["sitting"] = False
 				return self.action_list[self.action]
@@ -168,36 +168,36 @@ Have a great time at the party {}!!!""".format(username))
 			if not self.session.get("sitting", False):
 				return self.action_list[self.action]
 			else:
-				return "You have to stand up first, silly."
+				return "you have to stand up first, silly."
 		elif self.action == "walk":
 			if not self.session.get("sitting", False):
 				return self.action_list[self.action]
 			else:
-				return "You have to stand up first, silly."
+				return "you have to stand up first, silly."
 		elif self.action == "pick":
 			if not self.session.get("seen_key", False):
-				return "That doesn't work."
+				return "that doesn't work."
 			elif self.session.get("has_key", False):
-				return "You're already holding the key."
+				return "you're already holding the key."
 			else:
 				self.session["has_key"] = True
 				return self.action_list[self.action]
 		elif self.action == "unlock":
 			if not self.session.get("has_key", False):
-				return "You need a key to unlock the door!"
+				return "you need a key to unlock the door!"
 			elif self.session.get("has_unlocked_door", False):
-				return "The door's unlocked! It's just closed."
+				return "the door's unlocked! it's just closed."
 			else:
 				self.session["has_unlocked_door"] = True
 				return self.action_list[self.action]
 		elif self.action == "open":
 			if not self.session.get("has_unlocked_door", False):
-				return "The door's still locked."
+				return "the door's still locked."
 			else:
 				self.session["has_escaped"] = True
 				return self.action_list[self.action]
 		else:
-			return "Dunno what you're doing."
+			return "dunno what you're doing."
 
 if __name__ == '__main__':
 	start_game()
